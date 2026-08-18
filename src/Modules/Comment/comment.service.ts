62bdf427e8eae7aac37d3d6f076171d6408ba752
ahmed negm <ahmadnagm48@gmail.com>
2026-08-18 11:19:00 +0300

    Remove comment folder

--- src/Modules/Comment/comment.service.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { BadRequestException } from "../../Utils/response/error.response";
-import { PostModel } from "../../DB/Models/post.model";
-
-class CommentService {
-  constructor() {}
-}
-
-export default new CommentService();
